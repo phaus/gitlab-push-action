5d024b4fc6f41f69b5d7f01c753bf348b53df45e
Philipp Haußleiter <philipp@haussleiter.de>
2020-04-04 00:21:34 +0200

    still fixing host key

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,10 @@
 set -u -e -x
 
 # fix see https://stackoverflow.com/a/18079668
-rm -f /dev/tty
-mknod -m 666 /dev/tty c 5 0
 
 echo "Push $GITHUB_REPOSITORY to $GITLAB_REPOSITORY"
 mkdir -p ~/.ssh
-echo $SSH_PRIVATE_KEY | base64 -d > ~/.ssh/id_rsa
+echo "${SSH_PRIVATE_KEY}" | base64 -d > ~/.ssh/id_rsa
 
 chmod go-w ~/
 chmod 700 ~/.ssh
@@ -16,13 +14,13 @@ chmod 0600 ~/.ssh/id_rsa
 
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 ssh-keyscan -t rsa gitlab.com >> ~/.ssh/known_hosts
-ssh-keyscan -t rsa ${GITLAB_HOST} >> ~/.ssh/known_hosts
+ssh-keyscan -t rsa "${GITLAB_HOST}" >> ~/.ssh/known_hosts
 echo "" >> ~/.ssh/known_hosts
 echo ""
 
 ls -al ~/.ssh
 
-ssh -v git@${GITLAB_HOST}
+ssh -v "git@${GITLAB_HOST}"
 
 git checkout "${GITHUB_REF:11}"
 git-lfs install 
