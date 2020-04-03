88d5d1e979949bf36883c030b9a244bc7a3c2067
Philipp Haußleiter <philipp@haussleiter.de>
2020-04-03 23:25:16 +0200

    fixing .ssh access rights

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,19 +6,20 @@ echo "Push $GITHUB_REPOSITORY to $GITLAB_REPOSITORY"
 mkdir -p ~/.ssh
 echo $SSH_PRIVATE_KEY | base64 -d > ~/.ssh/id_rsa
 
-chmod go-w ~/
-chmod 700 ~/.ssh
-chmod 0600 ~/.ssh/id_rsa
 echo "adding host key:"
 echo $GITLAB_HOST_KEY | base64 -d
 echo ""
-echo $GITLAB_HOST_KEY | base64 -d  > ~/.ssh/known_hosts
+echo $GITLAB_HOST_KEY | base64 -d > ~/.ssh/known_hosts
+echo "" > ~/.ssh/known_hosts
+
+chmod go-w ~/
+chmod 700 ~/.ssh
+chmod 0600 ~/.ssh/id_rsa ~/.ssh/known_hosts
 
 git checkout "${GITHUB_REF:11}"
 git-lfs install 
 
 git remote add gitlab $GITLAB_REPOSITORY
-git push gitlab master
 git push gitlab $SRC_BRANCH:$DST_BRANCH
 
 echo "Git Version $(git describe --always --dirty --tags):"
