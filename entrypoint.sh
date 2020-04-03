817fce48b1a733e872a3e7edf72a5cdd07e3fbad
Philipp Haußleiter <philipp@haussleiter.de>
2020-04-03 23:17:02 +0200

    fixing hostkey verification

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,13 @@ echo "Push $GITHUB_REPOSITORY to $GITLAB_REPOSITORY"
 mkdir -p ~/.ssh
 echo $SSH_PRIVATE_KEY | base64 -d > ~/.ssh/id_rsa
 
+chmod go-w ~/
+chmod 700 ~/.ssh
+chmod 0600 ~/.ssh/id_rsa
 echo "adding host key:"
 echo $GITLAB_HOST_KEY | base64 -d
 echo ""
 echo $GITLAB_HOST_KEY | base64 -d  > ~/.ssh/known_hosts
-
-chmod go-w ~/
-chmod 700 ~/.ssh
-chmod 0600 ~/.ssh/id_rsa
 
 git checkout "${GITHUB_REF:11}"
 git-lfs install 
