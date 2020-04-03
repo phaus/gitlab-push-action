0163342ca8260bdca39b7bae09433aa4849ccd7d
Philipp Haußleiter <philipp@haussleiter.de>
2020-04-03 23:11:32 +0200

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,11 @@ set -u
 echo "Push $GITHUB_REPOSITORY to $GITLAB_REPOSITORY"
 mkdir -p ~/.ssh
 echo $SSH_PRIVATE_KEY | base64 -d > ~/.ssh/id_rsa
-echo $GITLAB_HOST_KEY | base64 -d  >> ~/.ssh/known_hosts
+
+echo "adding host key:"
+echo $GITLAB_HOST_KEY | base64 -d
+echo ""
+echo $GITLAB_HOST_KEY | base64 -d  > ~/.ssh/known_hosts
 
 chmod go-w ~/
 chmod 700 ~/.ssh
